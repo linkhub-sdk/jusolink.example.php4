6ec8f886228d76eb99f78b48c22fdf87cf27b784
JohnJeong123 <frenchofkiss@gmail.com>
2015-06-15 10:26:57 +0900

    modified Character-set(EUC-KR, UTF-8)

--- JusolinkExample/getBalance.php
+++ JusolinkExample/getBalance.php
@@ -1,8 +1,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=euc-kr" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<link rel="stylesheet" type="text/css" href="../Example.css" media="screen" />
-		<title>주소링크 SDK PHP 5.X Example.</title>
+		<title>주소링크 SDK PHP 4.X Example.</title>
 	</head>
 <?php
 	include 'common.php';
--- JusolinkExample/search.php
+++ JusolinkExample/search.php
@@ -1,8 +1,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=euc-kr" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<link rel="stylesheet" type="text/css" href="../Example.css" media="screen" />
-		<title>주소검색 SDK PHP 4.X Example.</title>
+		<title>주소링크 SDK PHP 4.X Example.</title>
 		<script type="text/javascript">
 			function openNewWindow(window) {
 				window_width = 500;
